167d60d21a3f2d365376946f735fb1bc6d7c17ea
Lucas Litter Mentz <lucaslittermentz@gmail.com>
2017-11-07 11:20:15 -0200

    Melhoria na impressão da main()

--- main.c
+++ main.c
@@ -25,6 +25,7 @@ int main(int argc, char * argv[])
 	fclose(yyin);
 
 	// IMPRIMIR TABELA DE SÍMBOLOS AQUI.
+	printf("\n");
 	printf("Símbolo    | Tipo\n");
 	printf("-----------------\n");
 	printTabSim(tabSim);
